dc349f4fdb05f070c22a53d33dbb0b5033a4c733
Devin Dixon <devinisafoal@gmail.com>
2017-04-17 11:12:36 -0600

    tests sort of working for nav

---
+++ test/nav.test.js
@@ -0,0 +1,38 @@
+import Nav from '../lib/components/Nav';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+describe('Nav', () => {
+  it('should start off with an empty string as the state', () => {
+    const nav = shallow(<Nav />);
+    const state = nav.state();
+    expect(state.location).toEqual('');
+  });
+
+  it('should change the state based on user input', () => {
+    const nav = shallow(<Nav />);
+    const state = nav.state();
+    expect(state.location).toEqual('');
+
+    const input = nav.find('.text');
+
+    input.simulate('change', { target: { value: 'kaneohe, hi' } });
+    expect(state.location).toEqual('kaneohe, hi');
+  });
+
+  it('should reset the state on click of submit', () => {
+    const nav = shallow(<Nav />);
+    const state = nav.state();
+    expect(state.location).toEqual('');
+
+    const input = nav.find('.text');
+
+    input.simulate('change', { target: { value: 'kaneohe, hi' } });
+    expect(state.location).toEqual('kaneohe, hi');
+
+    const submit = nav.find('.submit');
+
+    submit.simulate('click');
+    expect(state.location).toEqual('');
+  });
+});
