8e496a4e664f0969149bde0ad71737cd612a52e8
Devin Dixon <devinisafoal@gmail.com>
2017-04-17 10:31:01 -0600

    add test files

---
+++ test/nav-test.js
@@ -0,0 +1,17 @@
+import Nav from '../lib/components/Nav';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('Nav', () => {
+  it('should change the state based on user input', () => {
+    const nav = shallow(<Nav />)
+    // const input = nav.find('')
+  });
+
+
+
+
+
+
+
+});
