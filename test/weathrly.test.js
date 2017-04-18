56cbb247b8077534ad48b6e0a5cb3b3007bd91de
Kelly Miller <kellyannemiller@gmail.com>
2017-04-18 09:18:52 -0600

    fix autocomplete

--- test/weathrly.test.js
+++ test/weathrly.test.js
@@ -1,5 +1,8 @@
+
 import Weathrly from '../lib/components/Weathrly';
-import Hourly from '../lib/components/Hourly';
+import CurrentWeather from '../lib/components/CurrentWeather'
+import TenDay from '../lib/components/TenDay'
+import Nav from '../lib/components/Nav'
 import stub from './stubAPI.json';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
@@ -7,6 +10,21 @@ import { shallow, mount } from 'enzyme';
 
 
 describe('Weathrly', () => {
+  it('contains a current Weather component', () => {
+    const wrapper = shallow(<Weathrly />)
+    expect(wrapper.find('CurrentWeather').length).toBe(1)
+  })
+
+  it('contains an hourly component', () => {
+    const wrapper = shallow(<Weathrly />)
+    expect(wrapper.find('Hourly').length).toBe(1)
+  })
+
+  it('contains a ten day component', () => {
+    const wrapper = shallow(<Weathrly />)
+    expect(wrapper.find('TenDay').length).toBe(1)
+  })
+
   it('should have a default states of null', () => {
     const wrapper = shallow(<Weathrly />);
     const state = wrapper.state();
